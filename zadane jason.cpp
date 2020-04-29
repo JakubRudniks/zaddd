12db6aca86256a4e31f1cb5fc74ff51e12888483
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2020-04-29 10:41:19 +0200

    Add files via upload

---
+++ zadane jason.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int tab[1000];
+	int x=0; 	
+
+	ifstream wej;
+	ofstream wyj("wej.json");
+	
+	wej.open("we.txt");
+	wyj open("wyj.json");
+	if(wejscie.good())         
+	while(!wejscie.eof())  
+	 
+{
+           wejscie>>tab[x];
+           wyjscie<<tab[x];
+           x++;
+} 
+{
+		wejscie>>wyjscia>>" ";
+}
+{
+	wej close ();
+	wyj colse ();
+	
+	
+	return 0;
+}
